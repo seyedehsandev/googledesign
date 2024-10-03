47b4cefb70f3ba4ad21bfb692375f45068c43aa8
Ehsan <dev.ehsanhosseini@gmail.com>
2024-10-03 22:11:37 +0330

    adding favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "footer-text" : "#e8e8e8" ,
+      }
+    },
   },
   plugins: [],
 }
