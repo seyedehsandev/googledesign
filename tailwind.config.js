eff07d9340520e6858605dac3a9d35a01575ef1d
Ehsan <dev.ehsanhosseini@gmail.com>
2024-10-04 14:15:49 +0330

    search button designed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "footer-text" : "#e8e8e8" ,
+        "bottom-bg" : "#303134"
+      } ,
+      fontFamily: {
+        arial: ['Arial', 'sans-serif'],
       }
     },
   },
