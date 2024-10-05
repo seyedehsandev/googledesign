ec6d4a7397484245366a6c405482d48edd23f7d7
Ehsan <dev.ehsanhosseini@gmail.com>
2024-10-05 20:32:12 +0330

    adding styles to main and header sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "footer-text" : "#e8e8e8" ,
-        "bottom-bg" : "#303134"
+        "bottom-bg" : "#303134" , 
+        "bb-gg" : "#202124"  ,
+        "sign-in" : "#99c3ff"
       } ,
       fontFamily: {
         arial: ['Arial', 'sans-serif'],
